78754d3ad5b9ab7a039ee2707101e62e7e3793e3
Wojciech Wójcik <wojtek.wroclaw@hotmail.com>
2019-05-16 18:46:38 +0200

    v 0.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dpp_python_library_shopAssistant',
-    version='0.2.2',
+    version='0.2.3',
     packages=find_packages(),
     license='MIT',
     description='An example python package - DPP PWr ShopAssistant',
--- shopAssistant/yamlOperations/fileOperations.py
+++ shopAssistant/yamlOperations/fileOperations.py
@@ -4,7 +4,7 @@ import yaml
 def open_yaml(file):
     with open(file, 'r') as stream:
         try:
-            data = yaml.load_all(stream)
+            data = yaml.load(stream, Loader=yaml.Loader)
         except yaml.YAMLError as exc:
             print(exc)
         return data
